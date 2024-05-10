07eee2c6b7bc8de4c10aaef9a9c2865595e6f22d
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-05-10 17:45:05 +0600

    completed login functionality

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,52 @@
+import bcrypt from "bcrypt";
 import express, { Request, Response } from "express";
 import { check, validationResult } from "express-validator";
 import { generateJWTToken } from "../lib/utils";
 import User from "../models/User";
 
 const router = express.Router();
+
+// login user
+router.post(
+  "/login",
+  [
+    check("email", "Email is required").isString(),
+    check("password", "Password is required").isString(),
+  ],
+  async (req: Request, res: Response) => {
+    try {
+      const loginData = req.body;
+
+      const errors = validationResult(req);
+      if (!errors.isEmpty())
+        return res.status(400).json({ message: errors.array() });
+
+      const user = await User.findOne({ email: loginData.email });
+      if (!user) return res.status(400).json({ message: "User doesn't exist" });
+
+      const isMatchedPassword = await bcrypt.compare(
+        loginData.password,
+        user.password
+      );
+
+      if (!isMatchedPassword)
+        return res.status(400).json({ message: "Invalid password" });
+
+      const token = generateJWTToken(user?._id.toString()); // built-in utils function
+
+      // setting token to the browser cookie for authentication
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        expires: new Date(86400000),
+      });
+
+      res.json({ message: "User login successfull" });
+    } catch (error) {
+      console.log(__filename, error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
 
 // register user
 router.post(
@@ -15,19 +58,18 @@ router.post(
   ],
   async (req: Request, res: Response) => {
     try {
-      const userData = req.body;
-
+      // validate user data
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(400).json({ message: errors.array() });
       }
 
       // checking user exists or not using the email
-      const isUserExists = await User.findOne({ email: userData.email });
+      const isUserExists = await User.findOne({ email: req.body.email });
       if (isUserExists)
         return res.status(400).json({ message: "The email is already in use" });
 
-      const user = new User(userData);
+      const user = new User(req.body);
       await user.save();
 
       const token = generateJWTToken(user?._id.toString());
